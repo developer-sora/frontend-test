af89c1c16c257c92c5a37c116716f54b5bb7c705
developer-sora <sodaoov@gmail.com>
2025-04-21 02:04:06 +0900

    chore: 절대 경로 추가 및 전역 scss 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: [
+      {
+        find: "@styles",
+        replacement: path.resolve(__dirname, "src/styles"),
+      },
+    ],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@styles/_base/_variables";
+        `,
+      },
+    },
+  },
+});
